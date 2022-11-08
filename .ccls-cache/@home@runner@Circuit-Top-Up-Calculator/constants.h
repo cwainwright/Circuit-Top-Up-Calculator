f600285ee586d58a000ca5d1cc148f1bee8d6928
CW <cwainwright26@btinternet.com>
2022-11-08 02:00:36 +0000

    Removed replit files Updated .gitignore with replit files

--- .ccls-cache/@home@runner@Circuit-Top-Up-Calculator/constants.h
+++
@@ -1,8 +0,0 @@
-/* Cost for a single wash (in pence) */
-#define WASHPRICE 250
-
-/* Cost for a single dry (in pence) */
-#define DRYPRICE 130
-
-/* Minimum Top Up Amount (in pence) */
-#define MINTOPUP 500
--- .ccls-cache/@home@runner@Circuit-Top-Up-Calculator/monfunc.h
+++
@@ -1,9 +0,0 @@
-/* Monetary Function Declarations */
-
-int set_money(char *message);
-void get_money(int value);
-
-/* Calculation Function Declarations */
-int calculate_washes(int current_balance, int *washcount, int *drycount);
-int calculate_top_up(int remainder, int *washcount, int *drycount);
-int calculate_even_split(int *washcount, int *drycount);
